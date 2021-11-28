9d6a2c087ff87de7a8574a81719d5909e5f1b8fe
riverathom <95154621+riverathom@users.noreply.github.com>
2021-11-27 21:53:58 -0500

    Create Amistad.java

---
+++ Amistad.java
@@ -0,0 +1,19 @@
+
+public class Amistad
+{
+    private String usuario1;
+    private String usuario2;
+
+    public Amistad(String usuario1, String usuario2) {
+        this.usuario1 = usuario1;
+        this.usuario2 = usuario2;
+    }
+
+    public String getUsuario1() {
+        return usuario1;
+    }
+
+    public String getUsuario2() {
+        return usuario2;
+    }
+}
